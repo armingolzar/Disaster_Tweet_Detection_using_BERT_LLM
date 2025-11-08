67bca81cbe219571e09cf25d6eb2c2614de7bd31
Armin Golzar <armingolzar78@gmail.com>
2025-11-08 18:52:04 +0330

    Finalized FE mode and requirement is added

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 SEN_LENGTH = 100
 BATCH_SIZE = 32
-EPOCHS = 30
+EPOCHS = 25
 BERT_MODEL_NAME = "bert-base-uncased"
 
 TRAIN_PATH = ".\\data\\train.csv"
 TEST_PATH = ".\\data\\test.csv"
-PLOT_PATH = ".\\assets\\custom_training_loop_plot_fine_tune.png"
-MODEL_PATH = ".\\models\\disaster_detection_model_ft_custom_loop"
+PLOT_PATH = ".\\assets\\disaster_detection_training_feature_extraction.png"
+MODEL_PATH = ".\\models\\disaster_detection_model_feature_extraction"
 
 FIG_SIZE = (12, 5)
--- src/model.py
+++ src/model.py
@@ -15,20 +15,20 @@ class Disaster_Detection_BERT_Model():
 
         bert_model = TFBertModel.from_pretrained(config.BERT_MODEL_NAME)
 
-        bert_model.trainable = True
+        # bert_model.trainable = True
         # for var in bert_model.trainable_variables: 
         #     print(var.name)                        # this shows the full structure of the model and you can check what you want to freeze and what you dont 
 
-        for i, layer in enumerate(bert_model.bert.encoder.layer):
-            if i < 11:
-                layer.trainable = False
-            else:
-                layer.trainable = True  
+        # for i, layer in enumerate(bert_model.bert.encoder.layer):
+        #     if i < 11:
+        #         layer.trainable = False
+        #     else:
+        #         layer.trainable = True  
 
-        bert_model.bert.embeddings.trainable = False        
-        bert_model.bert.pooler.trainable = True
+        # bert_model.bert.embeddings.trainable = False        
+        # bert_model.bert.pooler.trainable = True
 
-        # bert_model.trainable = False
+        bert_model.trainable = False
 
             
 
@@ -45,21 +45,21 @@ class Disaster_Detection_BERT_Model():
         cls_embedding = bert_embedding[0][:, 0, :] # this is the raw cls embedding after multi-head-self-attention but pooler has an extra dense layer on this raw embeding
 
         drop1 = Dropout(0.2, name="drop1")(cls_embedding)
-        dense1 = Dense(512, activation="relu", name="dense1")(drop1)
+        dense1 = Dense(256, activation="relu", name="dense1")(drop1)
         norm1 = LayerNormalization(name="norm1")(dense1)
         drop2 = Dropout(0.2, name="drop2")(norm1)
-        dense2 = Dense(256, activation="relu", name="dense2")(drop2)
+        dense2 = Dense(128, activation="relu", name="dense2")(drop2)
         norm2 = LayerNormalization(name="norm2")(dense2)
         drop3 = Dropout(0.2, name="drop3")(norm2)
-        dense3 = Dense(128, activation="relu", name="dense3")(drop3)
+        dense3 = Dense(64, activation="relu", name="dense3")(drop3)
         norm3 = LayerNormalization(name="norm3")(dense3)
         drop4 = Dropout(0.2, name="drop4")(norm3)
         dense4 = Dense(32, activation="relu", name="dense4")(drop4)
-        norm4 = LayerNormalization(name="norm4")(dense4)
-        drop5 = Dropout(0.2, name="drop5")(norm4)
-        dense5 = Dense(16, activation="relu", name="dense5")(drop5)
+        # norm4 = LayerNormalization(name="norm4")(dense4)
+        drop5 = Dropout(0.2, name="drop5")(dense4)
+        # dense5 = Dense(16, activation="relu", name="dense5")(drop5)
 
-        output = Dense(1, activation="sigmoid", name="output")(dense5)
+        output = Dense(1, activation="sigmoid", name="output")(drop5)
 
         disaster_detection_model = Model(inputs=[input_tokens, input_masks], outputs=output)
         disaster_detection_model.summary()
